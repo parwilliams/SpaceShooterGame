e8edb2449c2688e335d867c4eed31e61902b2236
ConnorLWilliams <parwilliams8@gmail.com>
2022-03-03 13:29:19 -0600

    Enemy Created and Has Health
    Octopus enemy has animation and can be shot at and destroyed

--- SpaceShooterGame-Unity/Assets/Scripts/Ammo.cs
+++ SpaceShooterGame-Unity/Assets/Scripts/Ammo.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,16 +7,35 @@ public class Ammo : MonoBehaviour
 {
     public float Damage = 100f;
     public float LifeTime = 2f;
+    public Rigidbody2D rb;
     
     void OnEnable(){
         CancelInvoke();
         Invoke("Die", LifeTime);
+        //Debug.Log("Instantiated");
     }
 
-    void OnTriggerEnter2D(Collider2D Col){
+    /*
+    void OnTriggerEnter2D(Collider2D Col)
+    {
+        Debug.Log("Entered Collision");
         Health H = Col.gameObject.GetComponent<Health>();
 
-        if(H == null){
+        if (H == null)
+        {
+            return;
+        }
+
+        H.HealthPoints -= Damage;
+        Die();
+    }
+    */
+    void OnCollisionEnter2D(Collision2D collision) //Needed to change to collision, unsure of why
+    {
+        Health H = collision.gameObject.GetComponent<Health>();
+
+        if (H == null)
+        {
             return;
         }
 
